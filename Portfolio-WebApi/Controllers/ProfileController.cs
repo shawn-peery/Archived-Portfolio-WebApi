00e521c68a0a09ba6b792cfaa41dc3a9ac1d91e2
Shawn Peery <shawn.w8465@gmail.com>
2023-09-26 20:28:44 -0600

    Added Admin Role to check authorization

--- Portfolio-WebApi/Controllers/ProfileController.cs
+++ Portfolio-WebApi/Controllers/ProfileController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_WebApi.Controllers
 {
     public class ProfileController : Controller
     {
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public ActionResult<string> Profile()
         {
