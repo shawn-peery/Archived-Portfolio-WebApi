2fb8af754a588b19e318bbc7ab3bec8d436a635a
Shawn Peery <shawn.w8465@gmail.com>
2023-10-08 15:37:56 -0600

    Created Models
    - Will work on databases next

---
+++ Portfolio-WebApi/Models/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio_WebApi.Models
+{
+    public class Status
+    {
+        public string StatusId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
